05834cf2512becf86703380826ed4a6017e58cff
DANNOSIUCUTE <147748745+DANNOSIUCUTE@users.noreply.github.com>
2024-11-24 22:16:23 +0700

    Create InputPromptForm.py

---
+++ scr/form/InputPromptForm.py
@@ -0,0 +1,9 @@
+
+from flask_wtf import FlaskForm
+from wtforms import (StringField)
+
+
+class InputPromptForm(FlaskForm):
+    prompt = StringField("Prompt",  render_kw={"placeholder": "Prompt"})
+    newAPI = StringField("OpenAI API Key",  render_kw={
+                         "placeholder": "OpenAI API Key"})
