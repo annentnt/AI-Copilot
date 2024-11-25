a602842b7c43ed34dc04530ae09b4155c2129330
Goby <afcrichmond1897.official@gmail.com>
2024-11-25 18:39:51 +0700

    Add files via upload

---
+++ scr/script.js
@@ -0,0 +1,21 @@
+// Lấy các phần tử liên quan
+const loginTab = document.getElementById('login-tab');
+const registerTab = document.getElementById('register-tab');
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+
+// Gắn sự kiện click cho nút Login
+loginTab.addEventListener('click', () => {
+    loginTab.classList.add('active');
+    registerTab.classList.remove('active');
+    loginForm.classList.add('active');
+    registerForm.classList.remove('active');
+});
+
+// Gắn sự kiện click cho nút Register
+registerTab.addEventListener('click', () => {
+    registerTab.classList.add('active');
+    loginTab.classList.remove('active');
+    registerForm.classList.add('active');
+    loginForm.classList.remove('active');
+});
